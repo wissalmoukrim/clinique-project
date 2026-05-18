0cc3be9c14853dcee276b0a32343b9fcca45eabb
Wissal <wissalmoukrim9@gmail.com>
2026-05-18 14:48:02 +0100

    update backend

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='patient')
 
     # 🔐 sécurité login 
-    login_attempts = models.IntegerField(default=0)   # garde ton champ (compatibilité)
+    login_attempts = models.IntegerField(default=0)   
     is_locked = models.BooleanField(default=False)
 
    
